5e80d347fe3e6c75fa12a27855a6bb90eda0c02f
Siddhartha Basu <biosidd@gmail.com>
2023-01-25 05:05:13 -0600

    refactor: switched from template literal for animation name

--- src/StyledImage.ts
+++ src/StyledImage.ts
@@ -15,7 +15,7 @@ const StyledImage = styled("img", {
   width: "100%",
   height: "100%",
   "object-fit": fit,
-  animationName: `$materialize`,
+  animationName: "materialize",
   animationDuration: `${duration}ms`,
   animationTimingFunction: easing,
   "@keyframes materialize": {
