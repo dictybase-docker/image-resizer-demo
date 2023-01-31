14af206f9ff3a3b359ecb8260a0e79dfd21bdea5
Siddhartha Basu <biosidd@gmail.com>
2023-01-30 18:21:24 -0600

    refactor: use the global state the get the image dimension

--- src/ImageDisplay.tsx
+++ src/ImageDisplay.tsx
@@ -1,23 +1,37 @@
 import Box from "@mui/material/Box"
 import Image from "./Image"
-import { useAtomValue } from "jotai"
-import fileAtom from "./state"
-import percentWindowHeight from "./height"
+import { useAtomValue,useAtom } from "jotai"
+import {fileAtom, dimesionAtom} from "./state"
+import { percentWindowHeight, percentWindowWidth } from "./height"
+import {  useEffect, useRef } from "react"
+import Resizer from "./Resizer"
+
 
 const ImageDisplay = () => {
   const imageFile = useAtomValue(fileAtom)
+  const [dimension, setDimension] = useAtom(dimesionAtom)
+  const boxRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    setDimension({
+      width: percentWindowWidth(45),
+      height: percentWindowHeight(75),
+    })
+  }, [])
   return (
     <Box
-      sx={{ flexBasis: "45%", height: `${percentWindowHeight(75)}px` }}
+      sx={{
+        flexBasis: `${dimension.width}px`,
+        height: `${dimension.height}px`,
+        position: "relative",
+      }}
+      ref={boxRef}
       pt={3}>
-      {imageFile ? <Image src={imageFile} /> : null}
-    <Box sx={{ flexBasis: "45%", height: `${initialHeight}px` }} pt={3}>
-      {imageFile ? <Image src={imageFile} fit="fill" /> : null}
       {imageFile ? <Image src={imageFile} fit="fill" /> : <></>}
       {boxRef.current && imageFile ? (
         <Resizer element={boxRef.current} />
       ) : (
         <></>
+      )}
     </Box>
   )
 }
