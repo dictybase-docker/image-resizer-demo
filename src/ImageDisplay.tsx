be9ba1868f92e544bdbc8e43db3730973874393c
Siddhartha Basu <biosidd@gmail.com>
2023-01-30 18:19:38 -0600

    feat: connect resizer anchor point to the north east corner of the image

--- src/ImageDisplay.tsx
+++ src/ImageDisplay.tsx
@@ -13,6 +13,11 @@ const ImageDisplay = () => {
       {imageFile ? <Image src={imageFile} /> : null}
     <Box sx={{ flexBasis: "45%", height: `${initialHeight}px` }} pt={3}>
       {imageFile ? <Image src={imageFile} fit="fill" /> : null}
+      {imageFile ? <Image src={imageFile} fit="fill" /> : <></>}
+      {boxRef.current && imageFile ? (
+        <Resizer element={boxRef.current} />
+      ) : (
+        <></>
     </Box>
   )
 }
