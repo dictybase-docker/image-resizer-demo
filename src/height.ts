c509a21983370096b60dea76ef8e33dcaeeb3797
Siddhartha Basu <biosidd@gmail.com>
2023-01-30 18:17:01 -0600

    feat: add percent height function

--- src/height.ts
+++ src/height.ts
@@ -1,4 +1,7 @@
+const percentWindowWidth = (percent: number) =>
+  Math.round((percent * window.innerWidth) / 100)
+
 const percentWindowHeight = (percent: number) =>
   Math.round((percent * window.innerHeight) / 100)
 
-export default percentWindowHeight
+export { percentWindowHeight, percentWindowWidth }
